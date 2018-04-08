c4ad658664cf7649067180a6d630c48a058f4e12
Orgil Sugar <Orgil@engr2-5-206-dhcp.int.colorado.edu>
2018-04-08 13:07:57 -0600

    hi

---
+++ Database table sample/DataBase.sql
@@ -0,0 +1,4 @@
+create table if not exists user (Fname varchar(20), Lname varchar(20), Country varchar(20), sex char(1), Email varchar(20), Phone int(11),  bday DATE);
+
+create table if not exists guide( Fname varchar(20), Lname varchar(20), Country varchar(20), sex char(1), phone int(11), rating int(11), bday DATE);
+
